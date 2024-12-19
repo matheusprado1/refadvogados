6d971c23d6cd31179d7d443d8a420d19f545e923
matheusprado1 <matheusprado1096@gmail.com>
2024-12-19 15:24:32 -0300

    feat: SEO and favicon

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,20 +1,13 @@
 import { StyledImg } from './Logo.styles';
-import logoMobile from '../../assets/logo/logo-mobile.png';
+import logo from '../../assets/logo/logo-mobile.png';
 import { Link } from 'react-router-dom';
-
-// const StyledImg1 = styled.img`
-//   /* max-height: 10p; */
-//   /* padding-top: 10px; */
-//   max-height: 75px;
-//   cursor: pointer;
-// `;
 
 const Logo = () => {
   return (
     <Link to="/">
       <picture>
-        <source srcSet={logoMobile} media="(max-width: 768px)" />
-        <StyledImg src={logoMobile} alt="Logo" />
+        <source srcSet={logo} media="(max-width: 768px)" />
+        <StyledImg src={logo} alt="Logo" />
         {/* <StyledImg1 src={logo} alt="Logo" /> */}
       </picture>
     </Link>
